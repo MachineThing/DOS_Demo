8d20f621cdbb1c24cef46a94dd69f2dea4da2872
Mason Fisher <sackzombie10@gmail.com>
2022-04-12 16:38:15 -0400

    Plotted a pixel

--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
 int main() {
   mode_set(0x13);
+  plot_pixel();
+  while (1==1) {
+    /* code */
+  }
   return 0;
 }
--- src/vga.asm
+++ src/vga.asm
@@ -2,6 +2,7 @@
 
 section .text
 global _mode_set
+global _plot_pixel
 
 _mode_set:
   push bp
@@ -10,4 +11,12 @@ _mode_set:
   mov al, [bp + 4]
   int 10h
   pop bp
+  ret
+
+_plot_pixel:
+  mov ah, 0ch
+  mov al, 14
+  mov cx, 5
+  mov dx, 5
+  int 10h
   ret
