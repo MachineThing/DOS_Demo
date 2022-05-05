8f78ea5afc9e8fcbc664cc005944a8d86ea81522
Mason Fisher <sackzombie10@gmail.com>
2022-05-05 14:14:31 -0400

    Starting to read BMP soon

--- src/main.c
+++ src/main.c
@@ -1,31 +1,18 @@
 int coord = 0;
 
-char credit_file[144];
+char splash_file[17158];
 
 int main() {
-  /*mode_set(0x13);
+  mode_set(0x13);
 
-  while (coord < 200) {
-    plot_pixel(coord, coord, 12);
-    coord++;
+  file("SPLASH.BMP\0", 17158, splash_file);
+
+  if (splash_file[0] == 'B' && splash_file[1] == 'M') {
+    plot_pixel(10, 10, 14);
   }
 
-  plot_pixel(10, 10, 14);
-  plot_pixel(15, 10, 14);
-  plot_pixel(10, 11, 14);
-  plot_pixel(15, 11, 14);
-  plot_pixel(10, 13, 14);
-  plot_pixel(15, 13, 14);
-  plot_pixel(10, 14, 14);
-  plot_pixel(15, 14, 14);
-  plot_pixel(11, 15, 14);
-  plot_pixel(12, 15, 14);
-  plot_pixel(13, 15, 14);
-  plot_pixel(14, 15, 14);
-  while (1==1) {}*/
-
+  while (1==1) {}
   mode_set(0x03);
-  file("CREDIT.TXT\0", 144, credit_file);
-  puts(credit_file);
+
   return 0;
 }
